bf78ab7b586813a21203122162dc6f4eb57ec08d
Hamrouni Sarra <hamrounisarra5@gmail.com>
2022-08-10 22:39:46 +0200

    javascript

---
+++ js/contact.js
@@ -0,0 +1,51 @@
+function validateForm()                                    
+{ 
+    var name = document.forms["myForm"]["first-name"]; 
+    var name = document.forms["myForm"]["last-name"];               
+    var email = document.forms["myForm"]["email"];  
+    // var phone = document.forms["myForm"]["phone"];   
+    var message = document.forms["myForm"]["message"];   
+   
+    if (name.value == "")                                  
+    { 
+        document.getElementById('errorname').innerHTML="Veuillez entrez un nom valide";  
+        name.focus(); 
+        return false; 
+    }else{
+        document.getElementById('errorname').innerHTML="";  
+    }
+       
+    if (email.value == "")                                   
+    { 
+        document.getElementById('erroremail').innerHTML="Veuillez entrez une adresse mail valide"; 
+        email.focus(); 
+        return false; 
+    }else{
+        document.getElementById('erroremail').innerHTML="";  
+    }
+   
+    if (email.value.indexOf("@", 0) < 0)                 
+    { 
+        document.getElementById('erroremail').innerHTML="Veuillez entrez une adresse mail valide"; 
+        email.focus(); 
+        return false; 
+    } 
+   
+    if (email.value.indexOf(".", 0) < 0)                 
+    { 
+        document.getElementById('erroremail').innerHTML="Veuillez entrez une adresse mail valide"; 
+        email.focus(); 
+        return false; 
+    } 
+   
+    if (message.value == "")                           
+    {
+        document.getElementById('errormsg').innerHTML="Veuillez entrez un message valide"; 
+        message.focus(); 
+        return false; 
+    }else{
+        document.getElementById('errormsg').innerHTML="";  
+    }
+   
+    return true; 
+}
---
+++ js/home.js
@@ -0,0 +1,9 @@
+const flyBtn = document.querySelector('#fly');
+const hotelBtn = document.querySelector('#hotel');
+
+flyBtn.addEventListener('click' , () => {
+    var fly = document.getElementsByClassName("tab1") ;
+});
+hotelBtn.addEventListener('click' , () => {
+    var hotel = document.getElementsByClassName("tab2") ;
+});
